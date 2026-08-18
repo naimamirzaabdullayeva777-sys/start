c4d6033dfe603fe3e09155f3904c970c364c96c0
naimamirzaabdullayeva777-sys <naimamirzaabdullayeva777@gmail.com>
2026-08-18 15:01:31 +0500

    fix: add default storage backend for ImageField/FileField

--- config/settings.py
+++ config/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
